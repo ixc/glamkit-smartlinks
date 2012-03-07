41ae634379c0a1082ffe371e4fa7cbe6033cb306
George Karpenkov <george@metaworld.ru>
2012-03-07 15:20:07 +1100

    useful shortcuts for template filters

--- smartlinks/templatetags/smartlinks.py
+++ smartlinks/templatetags/smartlinks.py
@@ -6,9 +6,6 @@ from ..models import IndexEntry
 
 register = template.Library()
 
-link_parser = SmartLinkParser(smartlinks_conf)
-embed_parser = SmartEmbedParser(smartlinks_conf)
-
 @register.filter
 def smartlinks(value):
     """
@@ -17,18 +14,37 @@ def smartlinks(value):
     Replaces each smartlink with a corresponding
     ``<a href=...>...</a>`` link
     """
+    link_parser = SmartLinkParser(smartlinks_conf)
+    embed_parser = SmartEmbedParser(smartlinks_conf)
     for parser in (link_parser, embed_parser):
         value = parser.process_smartlinks(value)
     return value
 
 @register.filter
-def get_smartlinked_obj(value):
+def smartlink_obj(value):
     """
     Get the object the smartlink is referring to,
     or ``None``.
     """
+    link_parser = SmartLinkParser(smartlinks_conf)
     try:
         return link_parser.get_smartlinked_object(value)
+    except (IndexEntry.DoesNotExist,
+            IndexEntry.MultipleObjectsReturned):
+        return None
+
+@register.filter
+def smartlink_url(value):
+    """
+    Get the URL smartlinked object is referring to.
+    """
+    link_parser = SmartLinkParser(smartlinks_conf)
+
+    try:
+        obj = link_parser.get_smartlinked_object(value)
+        conf = link_parser.conf
+        url = getattr(obj, conf.url_field, None)
+        return url() if callable(url) else url
     except (IndexEntry.DoesNotExist,
             IndexEntry.MultipleObjectsReturned):
         return None
