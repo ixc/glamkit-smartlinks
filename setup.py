9231c41188b0b5120aa77edbbd7ce8e98b67cc95
Aram Dulyan <aram00@gmail.com>
2016-06-23 15:37:29 +1000

    Prepared packaging for 0.7.0 release.

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
+import setuptools
+import sys
 
-setup(name='glamkit-smartlinks',
-    version='0.6.1',
+if {'bdist_wheel', 'sdist'}.intersection(sys.argv):
+    long_description = open('README.rst').read()
+
+setuptools.setup(
+    name='glamkit-smartlinks',
+    use_scm_version={'version_scheme': 'post-release'},  # Get version from git tags.
     description='Conditional wiki-style links to Django models.',
-    author='Thomas Ashelford',
-    author_email='thomas@interaction.net.au',
-    url='http://glamkit.org/',
-    packages=find_packages(),
-    zip_safe=False,
+    author='Interaction Consortium',
+    author_email='studio@interaction.net.au',
+    url='https://github.com/ixc/glamkit-smartlinks',
+    long_description=locals().get('long_description', ''),
+    license='BSD',
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    setup_requires=['setuptools_scm'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
